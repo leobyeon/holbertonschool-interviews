a7a7a541a8652786a543e381a9abfd33509d62fe
leobyeon <386@holbertonschool.com>
2019-03-25 22:10:21 +0000

    Betty check 0

--- 0-binary.c
+++ 0-binary.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search - searches for a value in a 
+ * binary_search - searches for a value in a
  * sorted array of integers using the Binary search algorithm
  * @array: pointer to the first element of the array to search in
  * @size: the number of elements in array
